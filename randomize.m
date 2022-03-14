aac3b04cdef1e8c73b3a44283b4b6f0a2343b57d
mmazumder3699 <73852391+mmazumder3699@users.noreply.github.com>
2022-03-14 17:39:48 -0400

    Add files via upload

--- randomize.m
+++ randomize.m
@@ -1,12 +1,19 @@
 function dataset = randomize()
 %creates a folder of 100 random images from all of our video frame to make
 %diverse datasets for training
-mkdir dataset6
-for ii = 1:100
+mkdir dataset7
+ii = 1;
+while ii < 100
     number = randperm(32798, 1);
     number = num2str(number);
-    frame = dir(['*' number '*']);
+    frame = dir(['frame' number '.jpg']);
     frame = getfield(frame, {1}, 'name');
-    copyfile(frame, 'dataset1');
+    if isempty(frame)
+        ii = ii - 1;
+    else 
+        movefile(frame, 'dataset7');
+    end
+    ii = ii + 1;
+dataset = frame;
 end
 end
