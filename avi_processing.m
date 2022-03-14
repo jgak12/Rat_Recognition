4c6d5910ba1431b3716682f92ebb7b197b1cceb9
mmazumder3699 <73852391+mmazumder3699@users.noreply.github.com>
2022-03-14 15:29:39 -0400

    Add files via upload

--- avi_processing.m
+++ avi_processing.m
@@ -1,17 +1,19 @@
 %%read AVIs, break into frame by frame %%
-function still_frames = avi_processing(avi_file)
-video = VideoReader(avi_file);
+function still_frames = avi_processing(mp4_file)
+video = VideoReader(mp4_file);
+
+path = '/Users/mayamazumder/MATLAB/Projects/rats/videos';
 
 %loop through each frame and make jpg
 ii = 1;
 while hasFrame(video)
    frame = readFrame(video);
-   filename = [sprintf('ratvideo%d',ii) '.jpg'];
-   fullname = fullfile(workingDir, sprintf('images%d', avi_file),filename);
+   filename = [sprintf('frame%d',(ii + 14552)) '.jpg'];
+   fullname = fullfile(path, 'images' ,filename);
    imwrite(frame,fullname)    % Write out to a JPEG file (img1.jpg, img2.jpg, etc.)
    ii = ii+1;
 end
 %take everything in this folder and make it a cell array of images
-still_frames = dir(fullfile(workingDir,sprintf('images%d', avi_file),'*.jpg'));
+still_frames = dir(fullfile(path, 'images','*.jpg'));
 still_frames = {still_frames.name}';
 end
