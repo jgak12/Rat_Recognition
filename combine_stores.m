3c7f09856734e92d81628488d526edbc8b9b5127
jgak12 <john_maragakis@brown.edu>
2022-03-15 18:58:12 -0400

    Training Table Completed (hopefully it will work)

--- combine_stores.m
+++ combine_stores.m
@@ -1,14 +1,29 @@
 %COMBINE DATA STORES
 
-[n, ~] = size(gTruth.DataSource);
-newstr = 'FullDataSet';
-for i=1:n
-    tochange = gTruth.DataSource{i,1};
-    pieces = strsplit(tochange, '\');
-    pieces{1,7} = newstr;
-    together = strjoin(pieces,'\');
-    gTruth.DataSource{i,1} = groundTruthDataSource(together);
-end
+% [n, ~] = size(gTruth.DataSource);
+% newstr = 'FullDataSet';
+% for i=1:n
+%     q=class(gTruth.DataSource{1,1}) %lol says cant use brace indexing
+%     tochange = gTruth.DataSource{i,1};
+%     pieces = strsplit(tochange, '\');
+%     pieces{1,7} = newstr;
+%     together = strjoin(pieces,'\');
+%     gTruth.DataSource{i,1} = groundTruthDataSource(together);
+% end
 
 
+% load('ImageLabeled\Rayna_fixed.mat')
+% a= cell2table(gTruth.DataSource.Source)
+% load('ImageLabeled\Maya_fixed.mat')
+% b= cell2table(gTruth.DataSource.Source)
+% load('ImageLabeled\John_fixed.mat')
+% c= cell2table(gTruth.DataSource.Source)
 
+
+class(gTruth.LabelData)
+load('ImageLabeled\Rayna_fixed.mat')
+%a= cell2table(gTruth.LabelData)
+load('ImageLabeled\Maya_fixed.mat')
+%b= cell2table(gTruth.LabelData)
+load('ImageLabeled\John_fixed.mat')
+%c= cell2table(gTruth.LabelData)
