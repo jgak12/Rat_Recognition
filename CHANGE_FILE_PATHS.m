29124c3f46f6cdcebc394e390c301a3a7960a529
jgak12 <john_maragakis@brown.edu>
2022-03-15 14:05:44 -0400

    added maya and rayna's fixed datastores

--- CHANGE_FILE_PATHS.m
+++ CHANGE_FILE_PATHS.m
@@ -1,12 +1,12 @@
 %function newStore= fix_file_paths(img_path,label_file)
-    disp(gTruth.DataSource{2,1})
+    %disp(gTruth.DataSource{2,1})
     
     practice= gTruth
     practice.DataSource
     %for i=1:size(practice.DataSource,1)
     i=1;
         a= practice.DataSource{i,1};
-        currentpath= a(1:35); %specific to Rayna's file path, change later
+        currentpath= a(1:35); %specific to Rayna's file path, change later (35 for rayna?)
         b= a(37:end); %same as above^
         %newpath= fullfile('C:\Users\jmara\OneDrive\Desktop\Rat video series\FullDataSet\',b);
         newpath= 'C:\Users\jmara\OneDrive\Desktop\Rat video series\FullDataSet';
