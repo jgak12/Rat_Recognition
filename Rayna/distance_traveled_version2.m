ca6367a0a4a25af116e027d7e3ba1d6bfe64d831
raynapsimons <rayna_simons@brown.edu>
2022-03-23 14:29:18 -0400

    Commented distance_traveled_version2
    just fixed the comments lol

--- Rayna/distance_traveled_version2.m
+++ Rayna/distance_traveled_version2.m
@@ -1,22 +1,18 @@
-%function that will detect whether a rat is freezing based on its x and y
-%coordinates not changing
-
-
 
 function [total_distance] = distance_traveled_version2(table_x, start, finish)
-    working_table = table_x;
+    working_table = table_x; %use output from John's movement tracker
     total_distance = 0;
-    if finish > size(working_table,1)-1
-        finish = size(working_table,1)-1;
+    if finish > size(working_table,1)-1 
+        finish = size(working_table,1)-1; %makes sure that it's not trying to read off the end of the table
     end
     for i = start:finish
-        x1 = working_table{i,2};
-        y1 = working_table{i,3};
+        x1 = working_table{i,2}; %taking the value from the xlist column of the table
+        y1 = working_table{i,3}; %taking the value from the ylist column of the table
         x2 = working_table{(i+1),2};
         y2 = working_table{(i+1),3};
     
         distance = sqrt((x2 - x1)^2 + (y2 - y1)^2);
         total_distance = total_distance + distance;
     end
-    total_distance
+    total_distance %displays the total distance the rat moved over the course of the frames you selected
 end
